b383ed879449865bd8a2f771b041eb158e38326d
drag0nflames <drag0nflames@yahoo.com>
2015-11-05 13:59:53 +0545

    Fixed layout

--- app/src/main/java/com/lyang/demo/About.java
+++ app/src/main/java/com/lyang/demo/About.java
@@ -1,7 +1,9 @@
 package com.lyang.demo;
 
 import android.os.Bundle;
+import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
 
 /**
  * Created by DELL on 10/28/2015.
@@ -11,5 +13,20 @@ public class About extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.about);
+        ActionBar bar = getSupportActionBar();
+        if (bar != null) {
+            bar.setTitle("About");
+            bar.setDisplayShowHomeEnabled(true);
+            bar.setDefaultDisplayHomeAsUpEnabled(true);
+        }
+    }
+    
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                onBackPressed();
+        }
+        return true;
     }
 }
